ea00e996b1b8d7c9111a3a5973dbca95c27180e4
Jorik Cronenberg <jcronenberg@suse.de>
2020-02-14 19:40:19 +0100

    finish tilgungsplan

--- zinsrechner.cpp
+++ zinsrechner.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <math.h>
+#include <iomanip>
 
 using namespace std;
 
@@ -7,12 +9,13 @@ float eingabeBetrag(void);
 float berechneEndbetrag(float kapital, float zinssatz, int laufzeit);
 void berechneEndbetrag_jaehrlich(float kapital, float zinssatz, int laufzeit);
 float power(float, int);
-int tilgung(int, float, int);
+void tilgung(int, float, int);
+void eingabe(int, float&, float&, int&);
 
 int main() {
 	float kapital = 0.0;
 	float endkapital = 0.0;
-	float zinssatz = 0;
+	float zinssatz = 0.0;
 	int laufzeit = 0;
 	int auswahl = 0;
 	char nochmal = 'n';
@@ -24,37 +27,29 @@ int main() {
 
 		switch(auswahl) {
 		case 1:
-			cout << "Bitte geben Sie das Startkapital in Euro ein: ";
-			kapital = eingabeBetrag();
-			cout << "Bitte geben Sie den Zinssatz in \% ein: ";
-			zinssatz = eingabeBetrag();
-			cout << "Bitte geben Sie die Laufzeit in Jahren ein: ";
-			laufzeit = eingabeBetrag();
+			system("clear");
+			cout << "Sparplan berechnen\n\n";
+			eingabe(auswahl, kapital, zinssatz, laufzeit);
 			endkapital = berechneEndbetrag(kapital, zinssatz, laufzeit);
-			cout << endkapital << endl;
+			cout << "\nNach " << laufzeit << " Jahren: ";
+			cout << setprecision(2) << fixed << endkapital << endl;
 			break;
 		case 2:
-			cout << "Bitte geben Sie das Startkapital in Euro ein: ";
-			kapital = eingabeBetrag();
-			cout << "Bitte geben Sie den Zinssatz in \% ein: ";
-			zinssatz = eingabeBetrag();
-			cout << "Bitte geben Sie die Laufzeit in Jahren ein: ";
-			laufzeit = eingabeBetrag();
+			system("clear");
+			cout << "Sparplan mit jaehrlicher Ausgabe\n\n";
+			eingabe(auswahl, kapital, zinssatz, laufzeit);
 			berechneEndbetrag_jaehrlich(kapital, zinssatz, laufzeit);
 			break;
 		case 3:
-			cout << "Bitte geben Sie die Kreditsumme in Euro ein: ";
-			kapital = eingabeBetrag();
-			cout << "Bitte geben Sie den Zinssatz in \% ein: ";
-			zinssatz = eingabeBetrag();
-			cout << "Bitte geben Sie die Laufzeit in Jahren ein: ";
-			laufzeit = eingabeBetrag();
+			system("clear");
+			cout << "Tilgungsplan\n\n";
+			eingabe(auswahl, kapital, zinssatz, laufzeit);
 			tilgung(kapital, zinssatz, laufzeit);
 			break;
 		default:
 			cout << "Menuepunkt existiert nicht" << endl;
 		}
-		cout << "\n\n\nWollen Sie nochmal? <J/N> ";
+		cout << "\n\nWollen Sie nochmal? <J/N> ";
 		cin >> nochmal;
 
 	} while(nochmal == 'j' || nochmal == 'J');
@@ -76,31 +71,60 @@ int menue(void)
 	cin >> auswahl;
 	return auswahl;
 }
-int tilgung(int ksumme, float zinssatz, int laufzeit)
+void eingabe(int auswahl, float& kapital, float& zinssatz, int& laufzeit)
 {
-	float annuitaet;
-	float q;
+	if (auswahl != 3)
+		cout << "Bitte geben Sie die Startkapital in Euro ein: ";
+	else
+		cout << "Bitte geben Sie die Kreditsumme in Euro ein: ";
+	kapital = eingabeBetrag();
+	cout << "Bitte geben Sie den Zinssatz in \% ein: ";
+	zinssatz = eingabeBetrag();
+	cout << "Bitte geben Sie die Laufzeit in Jahren ein: ";
+	laufzeit = eingabeBetrag();
+}
+
+
+void tilgung(int ksumme, float zinssatz, int laufzeit)
+{
+	float q, qn;
+	float annuitaet, zinsanteil, tilgung, geszinsen;
+	float restschulden = ksumme;
+
+	cout << setprecision(2) << fixed;
 
 	zinssatz /= 100;
 	q = zinssatz + 1;
+	qn = power(q, laufzeit);
 
-	annuitaet = ksumme * ((zinssatz * power(q, laufzeit)) / (power(q, laufzeit) - 1));
+	annuitaet = ksumme * ((zinssatz * qn) / (qn - 1));
 
-
-	cout << "Jaehrliche Annuitaet: " << annuitaet;
+	cout << "Jaehrliche Annuitaet: " << annuitaet << endl;
+	cout << "\n\t\t\tZinsanteil\tTilgungsanteil\tRestschuld\n\n";
 
 	for(int i = 1; i <= laufzeit; i++) {
-		//zinsanteil = ksumme * ((q - 1) / (power(q, laufzeit) - 1)) * power(q, i - 1)
-		//tilgung
-		//restschulden
+		zinsanteil = ksumme * ((qn - power(q, i-1)) / (qn - 1)) * zinssatz;
+		geszinsen += zinsanteil;
+		tilgung = ksumme * ((q - 1) / (qn - 1)) * power(q, i-1);
+		restschulden -= tilgung;
+		if (i >= laufzeit)
+			restschulden = 0;
+
+		cout << "Nach dem " << i << ".Jahr:\t";
+		cout.width(10); cout << right << zinsanteil << "\t";
+		cout.width(14); cout << right << tilgung << "\t";
+		cout.width(10); cout << right << restschulden << endl;
 	}
 
-	return 0;
+	cout << "\nGesamte Zinsen: " << geszinsen << endl;
 }
 
 float power(float a, int b)
 {
 	float ergebnis = a;
+
+	if (!b)
+		return 1;
 
 	for (int i = 1; i < b; i++)
 		ergebnis *= a;
@@ -131,7 +155,7 @@ float berechneEndbetrag(float kapital, float zinssatz, int laufzeit)
 }
 void berechneEndbetrag_jaehrlich(float kapital, float zinssatz, int laufzeit)
 {
-	cout << "\n\n";
+	cout << setprecision(2) << fixed << "\n\n";
 
 	for(int i = 1; i <= laufzeit; i++) {
 		cout << "Nach dem " << i << ".Jahr:\t";
